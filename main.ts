8decbf8ef517f79e0044a001281694707f72fd82
jeons1qbs <jeons1@quarrybaykids.com>
2026-03-18 14:14:58 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,3 @@
-let a = 0
-let b = 0
 function showNumberLED (num: number) {
     basic.clearScreen()
     for (let index = 0; index <= num; index++) {
@@ -20,6 +18,23 @@ input.onGesture(Gesture.Shake, function () {
     a = 0
     b = 0
 })
+let b = 0
+let a = 0
+namespace CustomLEDs {
+
+    /**
+     * Lights up a specific number of LEDs in order.
+     * @param num the number of LEDs to turn on, eg: 5
+     */
+    //% block="show number LED $num"
+    export function showNumberLED(num: number): void {
+        basic.clearScreen()
+        for (let index2 = 0; index2 < num; index2++) {
+            led.plot(index2 % 5, Math.floor(index2 / 5))
+        }
+    }
+
+}
 basic.forever(function () {
 	
 })
